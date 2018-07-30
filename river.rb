edad240e119c4cb6383b398139c5630dd53b742e
samyuravikumar <samyuktha321@gmail.com>
2018-07-30 13:55:50 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,43 @@
 # river.rb
+class River
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  
+  attr_accessor :length, :countries, :discharge
+  
+  def flood
+    discharge = discharge * 1.3
+  end
+  
+  def dry_up
+    discharge = discharge * 0.5
+  end 
+  
+end
+
+nile = River.new("Nile")
+nile.countries = 11
+nile.length = 4258
+nile.discharge = 2830
+
+mississippi = River.new("Mississippi")
+mississippi.countries = 1
+mississippi.length = 2320
+mississippi.discharge = 593000
+
+amazon = River.new("Amazon")
+amazon.countries = 3 
+amazon.length = 4345
+amazon.discharge = 209000
+
+seine = River.new("Seine")
+seine.countries 
